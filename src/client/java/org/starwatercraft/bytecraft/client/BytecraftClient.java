53a566352b6383b232fa7a7205383507d59d80e3
Dmytro Vovk <53583824+VoDACode@users.noreply.github.com>
2025-03-12 23:15:14 +0200

    init

---
+++ src/client/java/org/starwatercraft/bytecraft/client/BytecraftClient.java
@@ -0,0 +1,10 @@
+package org.starwatercraft.bytecraft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BytecraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/starwatercraft/bytecraft/Bytecraft.java
@@ -0,0 +1,10 @@
+package org.starwatercraft.bytecraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Bytecraft implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
